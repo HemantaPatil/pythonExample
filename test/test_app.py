82ff8497eaf817f3e5c5b6190b68302c5b389580
Hemanta Patil <hemantapatil@Hemantas-MBP.attlocal.net>
2021-10-16 21:35:31 -0500

    correct hostname and port number

--- test/test_app.py
+++ test/test_app.py
@@ -1,6 +1,6 @@
 from app import index
 
-def test_index():
-    assert index() == "Chimmu welcome to my 1st Python web application, deployed on Heroku Cloud Platform"
+# def test_index():
+#    assert index() == "Chimmu welcome to my 1st Python web application, deployed on Heroku Cloud Platform"
 
 
